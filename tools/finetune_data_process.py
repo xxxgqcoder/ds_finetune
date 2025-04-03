3cbba2adca61fe65a45c2b28f4782b19fb45ee4a
xxxgqcoder <xxxgqcoder@gmail.com>
2025-04-03 10:06:22 +0800

    update

--- tools/finetune_data_process.py
+++ tools/finetune_data_process.py
@@ -63,22 +63,23 @@ def parse_zixun_data(data_path):
     return parsed_data
 
 
-# data path - parser function mapping
-# train
-parser_config = {
-    '/Users/xcoder/aDrive/dataset/law_finetune/CrimeKgAssitant_after_clean_52k.json': parse_52k_data,
-    '/Users/xcoder/aDrive/dataset/law_finetune/answer_with_law_92k.json': parse_92k_data,
-}
-processed_data_save_path = '/Users/xcoder/aDrive/dataset/law_finetune/finetune_processed_train.json'
-
-# # eval
 # parser_config = {
+#     '/Users/xcoder/aDrive/dataset/law_finetune/answer_with_law_92k.json':
+#     parse_92k_data,
+#     '/Users/xcoder/aDrive/dataset/law_finetune/CrimeKgAssitant_after_clean_52k.json':
+#     parse_52k_data,
 #     '/Users/xcoder/aDrive/dataset/law_finetune/fakao_gpt4.json':
 #     parse_fakao_data,
-#     '/Users/xcoder/aDrive/dataset/law_finetune/zixun_gpt4.json':
-#     parse_zixun_data,
 # }
-# processed_data_save_path = '/Users/xcoder/aDrive/dataset/law_finetune/finetune_processed_eval.json'
+# processed_data_save_path = '/Users/xcoder/aDrive/dataset/law_finetune/finetune_processed_train.json'
+
+
+parser_config = {
+    '/Users/xcoder/aDrive/dataset/law_finetune/zixun_gpt4.json':
+    parse_zixun_data,
+}
+processed_data_save_path = '/Users/xcoder/aDrive/dataset/law_finetune/finetune_processed_eval.json'
+
 
 processed_data = []
 for data_path, func in parser_config.items():
