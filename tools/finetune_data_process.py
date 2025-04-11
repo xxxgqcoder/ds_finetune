abd68bbaff1b23cf5d1367db78edde17ab1ef4b3
xxxgqcoder <xxxgqcoder@gmail.com>
2025-04-11 11:21:16 +0800

    add uuid column

--- tools/finetune_data_process.py
+++ tools/finetune_data_process.py
@@ -1,5 +1,8 @@
 import json
 
+import numpy as np
+from farmhash import FarmHash64
+
 # parse data into question and anwser pair
 
 
@@ -12,6 +15,7 @@ def parse_92k_data(data_path):
         ret = {
             'question': d['question'],
             'answer': d['answer'],
+            'uuid': FarmHash64(d['question']),
         }
         parsed_data.append(ret)
 
@@ -27,6 +31,7 @@ def parse_52k_data(data_path):
         ret = {
             'question': d['input'],
             'answer': d['output'],
+            'uuid': FarmHash64(d['input']),
         }
         parsed_data.append(ret)
 
@@ -42,6 +47,7 @@ def parse_fakao_data(data_path):
         ret = {
             'question': d['input'].strip('Question:'),
             'answer': d['output'],
+            'uuid': FarmHash64(d['input'].strip('Question:')),
         }
         parsed_data.append(ret)
 
@@ -57,27 +63,27 @@ def parse_zixun_data(data_path):
         ret = {
             'question': d['query'],
             'answer': d['response'],
+            'uuid': FarmHash64(d['query']),
         }
         parsed_data.append(ret)
 
     return parsed_data
 
 
-# parser_config = {
-#     '/Users/xcoder/aDrive/dataset/law_finetune/answer_with_law_92k.json':
-#     parse_92k_data,
-#     '/Users/xcoder/aDrive/dataset/law_finetune/CrimeKgAssitant_after_clean_52k.json':
-#     parse_52k_data,
-#     '/Users/xcoder/aDrive/dataset/law_finetune/fakao_gpt4.json':
-#     parse_fakao_data,
-# }
-# processed_data_save_path = '/Users/xcoder/aDrive/dataset/law_finetune/finetune_processed_train.json'
-
 parser_config = {
+    '/Users/xcoder/aDrive/dataset/law_finetune/answer_with_law_92k.json':
+    parse_92k_data,
+    '/Users/xcoder/aDrive/dataset/law_finetune/CrimeKgAssitant_after_clean_52k.json':
+    parse_52k_data,
+    '/Users/xcoder/aDrive/dataset/law_finetune/fakao_gpt4.json':
+    parse_fakao_data,
     '/Users/xcoder/aDrive/dataset/law_finetune/zixun_gpt4.json':
     parse_zixun_data,
 }
-processed_data_save_path = '/Users/xcoder/aDrive/dataset/law_finetune/finetune_processed_eval.json'
+
+processed_train_data_save_path = '/Users/xcoder/aDrive/dataset/law_finetune/finetune_processed_train.json'
+processed_eval_data_save_path = '/Users/xcoder/aDrive/dataset/law_finetune/finetune_processed_eval.json'
+eval_num = 1000
 
 processed_data = []
 for data_path, func in parser_config.items():
@@ -88,6 +94,19 @@ for data_path, func in parser_config.items():
     processed_data.extend(parsed_data)
 
 print(f'total {len(processed_data)} processed records')
-with open(processed_data_save_path, 'w', encoding='utf8') as f:
-    json.dump(processed_data, f, ensure_ascii=False, indent=4)
-    print(f'processed data saved to {processed_data_save_path}')
+np.random.shuffle(processed_data)
+
+train_data = processed_data[eval_num:]
+eval_data = processed_data[:eval_num]
+
+with open(processed_train_data_save_path, 'w', encoding='utf8') as f:
+    json.dump(train_data, f, ensure_ascii=False, indent=4)
+    print(
+        f'processed data saved to {processed_train_data_save_path}, total {len(train_data)}'
+    )
+
+with open(processed_eval_data_save_path, 'w', encoding='utf8') as f:
+    json.dump(eval_data, f, ensure_ascii=False, indent=4)
+    print(
+        f'processed data saved to {processed_eval_data_save_path}, total {len(eval_data)}'
+    )
